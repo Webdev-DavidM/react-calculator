91ed9ba30f934c8154fdf2edb2caa7ca6bc1d025
Webdev-DavidM <footballblubber@googlemail.com>
2020-11-06 16:57:44 +0000

    Updates

---
+++ precache-manifest.a901ef042687c10efd753a4e17e875ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea48e670d2f746527548cce1f581bc1",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "bf8248ef4afc2e5197d4",
+    "url": "/react-calculator/static/css/main.59b861ae.chunk.css"
+  },
+  {
+    "revision": "b18d21922345b256b057",
+    "url": "/react-calculator/static/js/2.352c7149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.352c7149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8248ef4afc2e5197d4",
+    "url": "/react-calculator/static/js/main.e61a2eeb.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
